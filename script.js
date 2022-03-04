c140650bf33132e01e5c7e857eb4872faf758b45
Victor-Igwe Blessed <blessedvictorigwe@gmail.com>
2022-03-05 00:08:44 +0100

    intialcommit

---
+++ script.js
@@ -0,0 +1,32 @@
+function burger(){
+    document.getElementById("mainImage").src="Images/Group 6.png" 
+    document.getElementById("BurgerImage").src="Images/Group 3.png" 
+    document.getElementById("PizzaImage").src="Images/Group 7 (3).png" 
+    document.getElementById("CakeImage").src="Images/Group 7 (2).png"
+}
+function pizza(){
+    document.getElementById("mainImage").src="Images/Group 6 (1).png" 
+    document.getElementById("PizzaImage").src="Images/Group 4.png" 
+    document.getElementById("BurgerImage").src="Images/Group 7 (1).png"
+    document.getElementById("CakeImage").src="Images/Group 7 (2).png"
+}
+function cake(){
+    document.getElementById("mainImage").src="Images/Group 6 (2).png" 
+    document.getElementById("CakeImage").src="Images/Group 5.png" 
+    document.getElementById("BurgerImage").src="Images/Group 7 (1).png"
+    document.getElementById("PizzaImage").src="Images/Group 7 (3).png" 
+}  
+window.onloadstart( document.getElementById("BurgerImage").src="Images/Group 3.png"  )
+
+function Login(){
+    alert("Login Successful")
+}
+function Register(){
+    alert("Registration Successful")
+}
+function offcanvas(){
+    document.getElementById("toggler").style.width="25px"
+}
+function offcanvasnorms(){
+document.getElementById("toggler").style.width="30px"
+}
