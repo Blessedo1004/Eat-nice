85342347cfac8d207b07835ec613d7d27378eae9
Blessedo1004 <93976441+Blessedo1004@users.noreply.github.com>
2022-05-11 16:13:06 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+function burger(){
+    document.getElementById("mainImage").src="Images/Group 6.png" 
+    document.getElementById("BurgerImage").src="Images/Group 3.png" 
+    document.getElementById("PizzaImage").src="Images/Group 7 (3).png" 
+    document.getElementById("CakeImage").src="Images/Group 7 (2).png"
+}
+function pizza(){
+    document.getElementById("mainImage").src="Images/Group 6 (1).png" 
+    document.getElementById("PizzaImage").src="Images/Group 4.png" 
+    document.getElementById("BurgerImage").src="Images/Group 7 (1).png"
+    document.getElementById("CakeImage").src="Images/Group 7 (2).png"
+}
+function cake(){
+    document.getElementById("mainImage").src="Images/Group 6 (2).png" 
+    document.getElementById("CakeImage").src="Images/Group 5.png" 
+    document.getElementById("BurgerImage").src="Images/Group 7 (1).png"
+    document.getElementById("PizzaImage").src="Images/Group 7 (3).png" 
+}  
+window.onloadstart( document.getElementById("BurgerImage").src="Images/Group 3.png"  )
+
+function Login(){
+    alert("Login Successful")
+}
+function Register(){
+    alert("Registration Successful")
+}
+function offcanvas(){
+    document.getElementById("toggler").style.width="1em"
+    document.getElementById("canvas-close").style.width="1em"
+    document.getElementById("canvas-close").style.height="1em"
+}
+function offcanvasnorms(){
+document.getElementById("toggler").style.width="1.5em"
+document.getElementById("canvas-close").style.height="0.5em"
+document.getElementById("canvas-close").style.width="0.5em"
+}
+function like(){
+    document.getElementById("like").src="Images/Vector (9).png"
+}
+
